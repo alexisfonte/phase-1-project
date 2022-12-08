03fc0801eaa89b5a34d30b5c6b1249718c2c6ea5
Alexis Fonte <alexis.e.fonte@gmail.com>
2022-12-08 12:52:12 -0600

    working dynamic ingredient and direction input forms

--- src/index.js
+++ src/index.js
@@ -18,14 +18,20 @@ const cardContainer = document.querySelector('div.search-result')
 const addIngredient = document.querySelector('.add')
 addIngredient.addEventListener('click', (e) => addNewIngredientInput(e))
 
+const addDirection = document.querySelector('.addDirection')
+addDirection.addEventListener('click', (e) => addNewDirectionInput(e))
+console.log(addDirection)
+
+//console.log(select)
+
 const form = document.querySelector('.new-recipe')
 form.addEventListener('submit', handleSubmit)
 
 // RenderFunctions
 function renderCards(recipeArray){
-    recipeArray.forEach(recipe => {
-        createCard(recipe)
-    })
+  recipeArray.forEach(recipe => {
+    createCard(recipe)
+  })
 }
 
 
@@ -36,102 +42,102 @@ function createCard(recipe){
   const recipeImage = recipe.image
   const ingredientList = recipe.ingredients
   const recipieInstruct = recipe.instructions
+  
+  const card = document.createElement('div')
+  card.setAttribute("class", "item")
+  
+  const recipeImageDOM = document.createElement('img')
+  recipeImageDOM.src = recipeImage
+  
+  
+  const detailsBtn = document.createElement('button')
+  detailsBtn.setAttribute("id", "details-button")
+  detailsBtn.setAttribute("class", recipeName)
+  detailsBtn.textContent = recipeName
+  
+  const ingredients = document.createElement('div')
+  ingredients.setAttribute('class', 'content')
+  
+  const ul = document.createElement('ul')
+  
+  ingredientList.forEach(ingredient => {
+    let ingredientName = ingredient.ingredient
+    let ingredientAmount = ingredient.amount
+    let ingredientUnit = ingredient.unit
     
-    const card = document.createElement('div')
-    card.setAttribute("class", "item")
+    const li = document.createElement('li')
+    li.textContent = `${ingredientAmount} ${ingredientUnit} ${ingredientName} `
+    ul.append(li)
+  })
+  
+  const ol = document.createElement('ol')
+  recipieInstruct.forEach(instruction => {
+    const li = document.createElement('li')
+    li.textContent = instruction.instruction
+    ol.append(li)
+  })
+  
+  const buttonsDiv = document.createElement('div')
+  buttonsDiv.setAttribute('class', 'buttonsDiv')
+  
+  const favoritesButton = document.createElement('button')
+  favoritesButton.setAttribute('class', 'favorites')
+  favoritesButton.textContent = "Add to Favorites"
+  favoritesButton.addEventListener('click', (e) => {
+    recipe.tags = !recipe.tags
+    favoritesButton.textContent = recipe.tags ? "Add to Favorites" : "Favorited"
     
-    const recipeImageDOM = document.createElement('img')
-    recipeImageDOM.src = recipeImage
-    
-    
-    const detailsBtn = document.createElement('button')
-    detailsBtn.setAttribute("id", "details-button")
-    detailsBtn.setAttribute("class", recipeName)
-    detailsBtn.textContent = recipeName
-    
-    const ingredients = document.createElement('div')
-    ingredients.setAttribute('class', 'content')
-    
-    const ul = document.createElement('ul')
-    
-    ingredientList.forEach(ingredient => {
-      let ingredientName = ingredient.ingredient
-      let ingredientAmount = ingredient.amount
-      let ingredientUnit = ingredient.unit
-      
-      const li = document.createElement('li')
-      li.textContent = `${ingredientAmount} ${ingredientUnit} ${ingredientName} `
-      ul.append(li)
+    fetch(`http://localhost:3000/recipes/${recipe.id}`,{
+      method: 'PATCH',
+      body: JSON.stringify({
+        tags: !recipe.tags
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
     })
+    .then(resp => resp.json())
     
-    const ol = document.createElement('ol')
-    recipieInstruct.forEach(instruction => {
-      const li = document.createElement('li')
-      li.textContent = instruction.instruction
-      ol.append(li)
+  })
+  
+  const deleteRecipe = document.createElement('button')
+  deleteRecipe.textContent = 'Delete'
+  deleteRecipe.addEventListener('click', (e) => {
+    fetch(`http://localhost:3000/recipes/${recipe.id}`,{
+      method: 'DELETE'
     })
-    
-    const buttonsDiv = document.createElement('div')
-    buttonsDiv.setAttribute('class', 'buttonsDiv')
-
-    const favoritesButton = document.createElement('button')
-    favoritesButton.setAttribute('class', 'favorites')
-    favoritesButton.textContent = "Add to Favorites"
-    favoritesButton.addEventListener('click', (e) => {
-      recipe.tags = !recipe.tags
-      favoritesButton.textContent = recipe.tags ? "Add to Favorites" : "Favorited"
-      
-      fetch(`http://localhost:3000/recipes/${recipe.id}`,{
-        method: 'PATCH',
-        body: JSON.stringify({
-          tags: !recipe.tags
-        }),
-        headers: {
-          'Content-type': 'application/json'
-        }
-      })
-      .then(resp => resp.json())
-      
-    })
-
-    const deleteRecipe = document.createElement('button')
-    deleteRecipe.textContent = 'Delete'
-    deleteRecipe.addEventListener('click', (e) => {
-      fetch(`http://localhost:3000/recipes/${recipe.id}`,{
-        method: 'DELETE'
-      })
-      .then(resp => resp.json())
-      card.remove()
-    })
-
-
-    card.append(recipeImageDOM)
-    card.append(detailsBtn)
-    detailsBtn.append(ingredients)
-    ingredients.append(ul)
-    ingredients.append(ol)
-    ol.append(buttonsDiv)
-    buttonsDiv.append(favoritesButton)
-    buttonsDiv.append(deleteRecipe)
-    cardContainer.append(card)
-    
-    // Functionality for the collapsible recipie details
-
-    let coll = document.getElementsByClassName(recipeName);
- 
-    for (let i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        if (ingredients.style.display === "block") {
-          ingredients.style.display = "none";
-        } else {
-          ingredients.style.display = "block";
-        }
-      });
-    }
-    
+    .then(resp => resp.json())
+    card.remove()
+  })
+  
+  
+  card.append(recipeImageDOM)
+  card.append(detailsBtn)
+  detailsBtn.append(ingredients)
+  ingredients.append(ul)
+  ingredients.append(ol)
+  ol.append(buttonsDiv)
+  buttonsDiv.append(favoritesButton)
+  buttonsDiv.append(deleteRecipe)
+  cardContainer.append(card)
+  
+  // Functionality for the collapsible recipie details
+  
+  let coll = document.getElementsByClassName(recipeName);
+  
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      if (ingredients.style.display === "block") {
+        ingredients.style.display = "none";
+      } else {
+        ingredients.style.display = "block";
+      }
+    });
+  }
+  
 }
- 
+
 
 function handleSubmit(e) {
   e.preventDefault()
@@ -148,17 +154,17 @@ function handleSubmit(e) {
       "Content-Type":"application/json",
       "Accept":"application/json",
     },
-        body: JSON.stringify({
-          name: newRecipeTitle,
-        image: newImage,
-        tags: false,
-        ingredients:  [{
-          ingredient: newIngredient,
-          amount: newAmount,
-          unit: ingredientMeasurement,
-        }],
-        instructions: [{
-          instruction: newDirections,
+    body: JSON.stringify({
+      name: newRecipeTitle,
+      image: newImage,
+      tags: false,
+      ingredients:  [{
+        ingredient: newIngredient,
+        amount: newAmount,
+        unit: ingredientMeasurement,
+      }],
+      instructions: [{
+        instruction: newDirections,
       }]
     })
   })
@@ -169,8 +175,8 @@ function handleSubmit(e) {
 
 function addNewIngredientInput(e){
   e.preventDefault()
-  console.log(e)
   const newInputSection = document.createElement('div')
+  newInputSection.setAttribute('id', 'new-ingredient')
   
   const ingredientInput = document.createElement('input')
   ingredientInput.setAttribute('class', 'ingredient')
@@ -179,13 +185,105 @@ function addNewIngredientInput(e){
   const amountInput = document.createElement('input')
   amountInput.setAttribute('class', 'amount')
   amountInput.setAttribute('placeholder', 'amount')
-
-
+  
+  const dropdown = document.createElement('select')
+  const  select = document.createElement('option')
+  select.setAttribute('value', 'select')
+  select.setAttribute('id', 'select')
+  select.textContent = select.value
+  const cups = document.createElement('option')
+  cups.setAttribute('value', 'cups')
+  cups.textContent = 'c'
+  const tbsp = document.createElement('option')
+  tbsp.setAttribute('value', 'tablespoons')
+  tbsp.textContent = 'tbsp'
+  const tsp = document.createElement('option')
+  tsp.setAttribute('value', 'teaspoons')
+  tsp.textContent = 'tsp'
+  const gal = document.createElement('option')
+  gal.setAttribute('value', 'gallon')
+  gal.textContent = 'gal'
+  const qt = document.createElement('option')
+  qt.setAttribute('value', 'quart')
+  qt.textContent = 'qt'
+  const pt = document.createElement('option')
+  pt.setAttribute('value', 'pint')
+  pt.textContent = 'pt'
+  const floz = document.createElement('option')
+  floz.setAttribute('value', 'fluid ounce')
+  floz.textContent = 'floz'
+  const ml = document.createElement('option')
+  ml.setAttribute('value', 'milliliters')
+  ml.textContent = 'ml'
+  const liters = document.createElement('option')
+  liters.setAttribute('value', 'liters')
+  liters.textContent = 'liters'
+  const oz = document.createElement('option')
+  oz.setAttribute('value', 'ounces')
+  oz.textContent = 'oz'
+  const lbs = document.createElement('option')
+  lbs.setAttribute('value', 'pounds')
+  lbs.textContent = "pounds"
+  const g = document.createElement('option')
+  g.setAttribute('value', 'grams')
+  g.textContent = 'g'
+  const ea = document.createElement('option')
+  ea.setAttribute('value', 'each')
+  ea.textContent = 'ea'
+  const none = document.createElement('option')
+  none.setAttribute('value', '')
+  none.textContent = ''
+  
   const deleteBtn = document.createElement('a')
-  deleteBtn.setAttribute('href', '#')  
+  deleteBtn.setAttribute('href', '#')
+  deleteBtn.setAttribute('id', 'delete-button')  
   deleteBtn.textContent = 'x'
+  deleteBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
+    deleteBtn.parentNode.remove()
+  })
+  
   newInputSection.append(ingredientInput)
   newInputSection.append(amountInput)
+  newInputSection.append(dropdown)
+  dropdown.append(select)
+  dropdown.append(cups)
+  dropdown.append(tbsp)
+  dropdown.append(tsp)
+  dropdown.append(gal)
+  dropdown.append(qt)
+  dropdown.append(pt)
+  dropdown.append(floz)
+  dropdown.append(ml)
+  dropdown.append(liters)
+  dropdown.append(oz)
+  dropdown.append(lbs)
+  dropdown.append(g)
+  dropdown.append(ea)
+  dropdown.append(none)
   newInputSection.append(deleteBtn)
   document.querySelector('.ingredient-section').appendChild(newInputSection)
+}
+
+function addNewDirectionInput(e){
+  e.preventDefault()
+  const newInputSection = document.createElement('div')
+  newInputSection.setAttribute('id', 'new-direction')
+  
+  const directionInput = document.createElement('input')
+  directionInput.setAttribute('class', 'direction')
+  directionInput.setAttribute('placeholder', 'Directions:')
+
+  const deleteBtn = document.createElement('a')
+  deleteBtn.setAttribute('href', '#')
+  deleteBtn.setAttribute('id', 'delete-button')  
+  deleteBtn.textContent = 'x'
+  deleteBtn.addEventListener('click', (e) =>{
+    e.preventDefault()
+    deleteBtn.parentNode.remove()
+  })
+
+  newInputSection.append(directionInput)
+  newInputSection.append(deleteBtn)
+  document.querySelector('.addDirections').append(newInputSection)
 }
